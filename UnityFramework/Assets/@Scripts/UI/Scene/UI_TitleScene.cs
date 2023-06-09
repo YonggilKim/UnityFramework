48b1eed4bebac3504e87ca6b001fc3830511ddff
yonggil kim <gtmddls41@gmail.com>
2023-06-09 17:10:30 +0900

    프로젝트생성

---
+++ UnityFramework/Assets/@Scripts/UI/Scene/UI_TitleScene.cs
@@ -0,0 +1,50 @@
+using Data;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using TMPro;
+using static Define;
+
+public class UI_TitleScene : UI_Scene
+{
+    #region Enum
+    enum GameObjects
+    {
+    }
+
+    enum Buttons
+    {
+    }
+
+    enum Texts
+    {
+    }
+    #endregion
+
+
+    public override bool Init()
+    {
+        if (base.Init() == false)
+            return false;
+
+        //BindObject(typeof(GameObjects));
+        //BindButton(typeof(Buttons));
+        //BindText(typeof(Texts));
+        
+        return true;
+    }
+
+    private void Awake()
+    {
+        Init();
+    }
+    private void Start()
+    {
+       
+    }
+
+
+}
